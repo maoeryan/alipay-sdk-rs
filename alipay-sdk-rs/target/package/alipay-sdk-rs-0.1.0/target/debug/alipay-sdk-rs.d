c9f903d4946563a8960a8fa22195d4c6046f3026
maoeryan <wy@qvar.cn>
2022-06-09 10:23:18 +0800

    init

--- alipay-sdk-rs/target/package/alipay-sdk-rs-0.1.0/target/debug/alipay-sdk-rs.d
+++
@@ -1,1 +0,0 @@
-/Users/guoshikeji/git/maoeyan/alipay-sdk-rs/target/package/alipay-sdk-rs-0.1.0/target/debug/alipay-sdk-rs: /Users/guoshikeji/git/maoeyan/alipay-sdk-rs/target/package/alipay-sdk-rs-0.1.0/src/main.rs
